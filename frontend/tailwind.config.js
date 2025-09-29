65de94490d7362f0ad2b7eb377a93d64f3c2ed25
Manas Sharma <manass@Manass-MacBook-Pro.local>
2025-09-29 18:10:44 -0400

    Add frontend as regular directory

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'tata-blue': '#4E84C4',
+      },
+    },
+  },
+  plugins: [],
+}
